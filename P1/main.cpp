65e44d5ca12e4057b9bbb647c8f0aac28cfcd731
lvas463 <lvas463@gmail.com>
2022-11-28 16:45:11 -0500

    Added a description and properly organized project files.

---
+++ P1/main.cpp
@@ -0,0 +1,7 @@
+//
+// Created by Lucas Vas on 11/28/2022.
+//
+
+// We are going to create an "event simulation" program. All of these events should be created and prioritized through
+// their timestamp. The queue will act as a clock and will be loaded with some initial events.
+// Refer to the "Discrete Event Simulation" section in the Class 11 slides.
